0efa1dcbc065d131dc196391177d68bb1383c86b
Jan-Willem Baart <jwbaart@gmail.com>
2020-11-01 22:16:38 +0100

    fix: basic layout

---
+++ components/Layout.tsx
@@ -0,0 +1,18 @@
+import { Box, Button } from "@material-ui/core";
+import React from "react";
+import PageFooter from "./PageFooter";
+import PageHeader from "./PageHeader";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <PageHeader></PageHeader>
+      <Box component="span" m={1}>
+        {children}
+      </Box>
+      <PageFooter></PageFooter>
+    </div>
+  );
+};
+
+export default Layout;
