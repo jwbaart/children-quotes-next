3b81b628a02c84bb8c2be6b01feff69c157a81a0
Jan-Willem Baart <jwbaart@gmail.com>
2021-01-08 13:58:45 +0100

    feat: basis graphql implementation (#9)

---
+++ apollo/resolvers.ts
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+
+export const resolvers = {
+  Query: {
+    rates: async (_root, { currency, numberOfItems = 5 }) => {
+      try {
+        const results = await fetch(
+          `https://api.coinbase.com/v2/exchange-rates?currency=${currency}`
+        );
+        const response = await results.json();
+        const exchangeRates = _.map(response.data.rates, (rate, currency) => ({
+          currency,
+          rate,
+        }));
+        return exchangeRates.length > numberOfItems
+          ? exchangeRates.slice(0, numberOfItems)
+          : exchangeRates;
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    viewer(_parent, _args, _context, _info) {
+      return { id: 1, name: 'John Smith', status: 'cached' };
+    },
+  },
+  ExchangeRate: {
+    name: async ({ currency }) => {
+      try {
+        const results = await fetch('https://api.coinbase.com/v2/currencies');
+        const currencyData = await results.json();
+
+        const currencyInfo = currencyData.data.find(
+          (c) => c.id.toUpperCase() === currency
+        );
+        return currencyInfo ? currencyInfo.name : null;
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import NextLink from 'next/link';
 import Copyright from '../src/Copyright';
 import Layout from '../components/Layout';
 import { useFetchUser } from '../utils/user';
+import { ExchangeRates } from '../components/ExchangeRates';
+import { Viewer } from '../components/Viewer';
 
 const Index: React.FC = (): JSX.Element => {
   const { user, loading } = useFetchUser();
@@ -18,6 +20,8 @@ const Index: React.FC = (): JSX.Element => {
           <Typography variant="h4" component="h1" gutterBottom>
             Next.js example
           </Typography>
+          <Viewer></Viewer>
+          <ExchangeRates></ExchangeRates>
           <NextLink href="/about">To about</NextLink>
           <ProTip />
           <Copyright />
