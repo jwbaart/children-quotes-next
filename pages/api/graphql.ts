e012fbce89ac36bfa6fa9ed979cae8e0ee52b342
Jan-Willem Baart <jwbaart@gmail.com>
2021-01-12 16:41:31 +0100

    chore: revert introspection fix

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -7,7 +7,7 @@ const apolloServer = new ApolloServer({
    * Add introspection to view the schema / docs in playground after running `next build` and `next start`
    * Without introspection, you'll get a message saying "Server cannot be reached"
    */
-  introspection: process.env['VERCEL_ENV'] !== 'production',
+  introspection: true,
   playground: {
     settings: {
       'request.credentials': 'same-origin',
