5ba93b73bd1d6172633aebc065f6fdfd40da673e
Jan-Willem Baart <jwbaart@gmail.com>
2021-01-14 11:51:18 +0100

    fix: quotes UI

--- components/quotes/QuoteCard.tsx
+++ components/quotes/QuoteCard.tsx
@@ -1,3 +1,12 @@
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+} from '@material-ui/core';
+
 export interface QuoteCardProps {
   title: string;
   text: string;
@@ -10,10 +19,19 @@ export const QuoteCard: React.FunctionComponent<QuoteCardProps> = ({
   authorName,
 }) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{text}</p>
-      <p>Author: {authorName}</p>
-    </div>
+    <Card variant="outlined">
+      <CardHeader
+        avatar={<Avatar aria-label="recipe">R</Avatar>}
+        title={title}
+        subheader="September 14, 2016"
+      />
+      <CardContent>
+        <p>{text}</p>
+        <p>Author: {authorName}</p>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 };
--- components/quotes/Quotes.tsx
+++ components/quotes/Quotes.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client';
+import { Grid } from '@material-ui/core';
 import { QUOTES } from '../../apollo/quotes/queries';
 import { QuoteCard } from './QuoteCard';
 
@@ -9,14 +10,19 @@ export const Quotes: React.FunctionComponent = () => {
   if (error) return <p>Error :(</p>;
 
   if (!loading && !error)
-    return data.quotes.map(({ title, text, author }, i) => {
-      return (
-        <QuoteCard
-          title={title}
-          text={text}
-          authorName={author.name}
-          key={i}
-        ></QuoteCard>
-      );
-    });
+    return (
+      <Grid container spacing={3}>
+        {data.quotes.map(({ title, text, author }, i) => {
+          return (
+            <Grid item xs={12} sm={6} md={4} lg={4} xl={4} key={i}>
+              <QuoteCard
+                title={title}
+                text={text}
+                authorName={author.name}
+              ></QuoteCard>
+            </Grid>
+          );
+        })}
+      </Grid>
+    );
 };
